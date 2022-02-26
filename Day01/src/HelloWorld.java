2170684fa0b9dd66e53609d3cc956f239aea3446
Harry JongSeok Lee <lodossw@gmail.com>
2022-02-26 14:54:59 +0900

    주석설명

---
+++ Day01/src/HelloWorld.java
@@ -0,0 +1,25 @@
+
+// 1. 주석문 
+// 컴파일러는 해석을 안하고 넘어가는 문장들 
+// 사용목적은 : 개발자(사람)이 읽고 참고하도록 써넣은 정보.
+
+// a) 한줄주석 
+// <-- 한줄주석 
+// b) 여러줄 주석
+/*
+   여러줄 주석입니다.
+   냐옹냐옹 
+   Author : lodossw@gmai.com
+   Action : 살려줘
+ */
+
+
+// HelloWorld 클래스 
+public class HelloWorld {
+
+	// HelloWorld 프로그램의 시작포인트 
+	public static void main(String[] args) {
+		System.out.println("하악하악, HelloWorld");
+	}
+	
+}
